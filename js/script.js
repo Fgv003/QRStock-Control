2108d3bde1537628aa674d116445318e61c36985
Érick Nunes <ericknp15@gmail.com>
2024-05-18 03:49:31 -0300

    Cadastro e Login

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 //LINK REF: https://www.youtube.com/watch?v=oQ3_p4oCzDE&t=52s
 
-
-
 const botaoPdf = document.querySelector("#button-pdf");
 
 botaoPdf.addEventListener("click", () => {
@@ -116,41 +114,7 @@ async function cadastrarProduto() {
 }
 
 
-//LINK REF: https://www.youtube.com/watch?v=D_f7rGBMuPI
 
-async function cadastrarUser() {
-    let userName = document.getElementById('userName').value;
-    let userEmail = document.getElementById('userEmail').value;
-    let userPassword = document.getElementById('userPassword').value;
-    let phoneNumberUser = document.getElementById('phoneNumberUser').value;
-
-    const user = {
-        userName,
-        userEmail,
-        userPassword,
-        phoneNumberUser,
-    };
-
-    try {
-        let response = await fetch('https://662eceed43b6a7dce30dce42.mockapi.io/user', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(user),
-        });
-
-        if (response.ok) {
-            console.log('Usuário cadastrado com sucesso!');
-            window.location.href = "index.html";
-        } else {
-            console.error('Erro ao cadastrar usuário:', await response.text());
-        }
-
-    } catch (error) {
-        console.error('Erro ao cadastrar usuário:', error);
-    }
-}
 
 function carregarEtiqueta() {
 
@@ -191,27 +155,9 @@ function carregarEtiqueta() {
 }
 
 
-//Em desenvolvimento
-function login() {
-    
-}
-
-
-
-
-
-
-
-
-
-/*
-       TESTANDO ABAIXO:
-*/
-
-
 // LINK REF: https://github.com/davidshimjs/qrcodejs?utm_source=cdnjs&utm_medium=cdnjs_link&utm_campaign=cdnjs_library
 
-//script de acesso a biblioteca
+//script de acesso a biblioteca:
 //<script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
 
 
---
+++ js/script_User_Cadastro.js
@@ -0,0 +1,98 @@
+//Cadastro:
+
+document.addEventListener("DOMContentLoaded", function () {
+    var formErrorElement = document.querySelector('.form-error');
+    if (formErrorElement) {
+        formErrorElement.value = '';
+    }
+
+    function validarFormulario() {
+        var userName = document.getElementById('userName').value;
+        var userEmail = document.getElementById('userEmail').value;
+        var userPassword = document.getElementById('userPassword').value;
+        var passwordConfirm = document.getElementById('password_confirm').value;
+        var phoneNumberUser = document.getElementById('phoneNumberUser').value;
+
+
+
+        if (!userName || !userEmail || !userPassword || !phoneNumberUser || userPassword !== passwordConfirm) {
+            if (userPassword !== passwordConfirm) {
+                window.alert('As senhas não coincidem. Por favor, verifique e tente novamente.');
+                return false;
+            } else {
+                window.alert('Por favor, preencha todos os campos corretamente.');
+                return false;
+            }
+        }
+        var usuario = {
+            "userName": userName,
+            "userEmail": userEmail,
+            "userPassword": userPassword,
+            "phoneNumberUser": phoneNumberUser,
+        };
+
+        return usuario;
+    }
+
+    document.querySelector('.btn-signup').addEventListener('click', function (event) {
+        event.preventDefault();
+        let resp = validarFormulario();
+        if (resp) {
+            fetch('https://662eceed43b6a7dce30dce42.mockapi.io/user', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(resp)
+            })
+                .then(() => window.location.href = 'index.html')
+                .catch(error => console.error('Erro ao enviar formulário:', error));
+        } else {
+            console.log('Formulário não foi validado corretamente.');
+        }
+    });
+});
+
+
+
+//Login:
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const form = document.querySelector('.form-login');
+
+    if (!form) return;
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const userEmail = document.getElementById('userEmail').value;
+        const userPassword = document.getElementById('userPassword').value;
+
+        fetch(`https://662eceed43b6a7dce30dce42.mockapi.io/user?userEmail=${userEmail}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                const usuario = data.find(user => user.userPassword === userPassword);
+                if (usuario) {
+                    console.log("Usuário encontrado:", usuario);
+                    window.location.href = 'index.html';
+                } else {
+                    const nomeErrorElement = document.getElementById('nome-error');
+                    if (nomeErrorElement) {
+                        nomeErrorElement.textContent = 'Senha inválida.';
+                    }
+                }
+            })
+            .catch((error) => {
+                console.error('Erro na requisição:', error);
+                const nomeErrorElement = document.getElementById('nome-error');
+                if (nomeErrorElement) {
+                    nomeErrorElement.textContent = 'Usuário inválido.';
+                }
+            });
+    });
+});
